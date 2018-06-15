860e78eaf9a38817afda44127e750e93028efd9a
NehalPatel <nehal.sdjic@gmail.com>
2018-06-15 16:08:25 +0530

    Initial Setup

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        try
+        {
+
+            $socialUser = Socialite::driver($provider)->user();
+
+        } catch (\Exception $e) {
+
+            return redirect('/');
+
+        }
+
+        $socialProvider = SocialProvider::where('provider_id', $socialUser->getId()->first());
+
+        if(!$socialProvider)
+        {
+            $user = User::firstOrCreate(
+                ['email' => $socialUser->getEmail() ],
+                ['name' => $socialUser->getName() ]
+            );
+
+            $user->socialProvider()->create(
+                ['provider_id' =>$socialUser->getId(), 'provider' => $provider]
+            );
+        }
+        else
+        {
+            $user = $socialProvider->user;
+        }
+
+        auth()->login($user);
+
+        return redirect('/home');
+    }
+}
